bc1b4c2d69c349d894b852a2e608e7a83c9302d6
Kaillooh <pierre.etheve.lfdv@gmail.com>
2019-02-24 18:21:08 +0100

    Prestashop install

---
+++ Prestashop/app/cache/prod/appParameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'Prestashop',
+    'database_user' => 'phpmyadmin',
+    'database_password' => 'hector44',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'OSD2yDJm5KDABOkWNkSqfpmyfpGpDNsXu6CL2B7WTzrBMZFrAZIOVdpr',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2019-02-24',
+    'locale' => 'en-US',
+    'cookie_key' => 'rPtjfs1nvW2W4wAUotIHE0aM9n7ok342xVjfLoQ8TN7CRuNKBmusdIyU',
+    'cookie_iv' => 'OT5DWhP4',
+    'new_cookie_key' => 'def00000eadc7dec38198544a1a5fd49b933738634b67b5a5ff0d6197ab72d25a7aea62c7075db123b515e407c39275ca9b16446d8e7ab2cbbf5ecfcda9accc2e1ec5a85',
+  ),
+);
