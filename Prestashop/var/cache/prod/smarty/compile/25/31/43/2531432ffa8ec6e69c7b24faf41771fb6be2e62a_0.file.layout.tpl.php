454ab53e9ff4ed11c14380a29a28eecaab739953
Kaillooh <pierre.etheve.lfdv@gmail.com>
2019-03-05 22:46:22 +0100

    Revert "Removed Prestashop"
    This reverts commit 2f4c42250079e99e482a12046ae20a4483d9caa2.

---
+++ Prestashop/var/cache/prod/smarty/compile/25/31/43/2531432ffa8ec6e69c7b24faf41771fb6be2e62a_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-02-24 19:07:51
+  from '/home/kaillooh/Documents/GarageProject/Prestashop/admin321svg0mk/themes/default/template/controllers/login/layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c72dd771e9626_91167075',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2531432ffa8ec6e69c7b24faf41771fb6be2e62a' => 
+    array (
+      0 => '/home/kaillooh/Documents/GarageProject/Prestashop/admin321svg0mk/themes/default/template/controllers/login/layout.tpl',
+      1 => 1551031431,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c72dd771e9626_91167075 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
---
+++ Prestashop/var/cache/prod/smarty/compile/b1/64/6e/b1646e43b51a257b1ad2c351a8f8f4aa8965d4b7_0.file.content.tpl.php
@@ -0,0 +1,33 @@
+<?php
+/* Smarty version 3.1.33, created on 2019-02-24 19:09:17
+  from '/home/kaillooh/Documents/GarageProject/Prestashop/admin321svg0mk/themes/new-theme/template/content.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.33',
+  'unifunc' => 'content_5c72ddcdcc4739_82976113',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'b1646e43b51a257b1ad2c351a8f8f4aa8965d4b7' => 
+    array (
+      0 => '/home/kaillooh/Documents/GarageProject/Prestashop/admin321svg0mk/themes/new-theme/template/content.tpl',
+      1 => 1551031431,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5c72ddcdcc4739_82976113 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div id="ajax_confirmation" class="alert alert-success" style="display: none;"></div>
+
+
+<?php if (isset($_smarty_tpl->tpl_vars['content']->value)) {?>
+  <?php echo $_smarty_tpl->tpl_vars['content']->value;?>
+
+<?php }
+}
+}
